0afd7fa13a84bcaad52381af940ccde42508c433
anastasiia-hrekova <anastasia.grekova1@gmail.com>
2024-11-07 12:21:32 +0200

    fist commit

---
+++ js/navigation.js
@@ -0,0 +1,11 @@
+const button = document.querySelector('.main-page__btn');
+const section = document.querySelector('.navigation');
+
+const toggleOpen = () => section.classList.toggle('hidden');
+
+button.addEventListener('click', toggleOpen);
+button.addEventListener('click', toggleOpen);
+
+
+
+     
